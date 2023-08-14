0f81c4118ea1e2a5e559ead6607a56408426a7b9
Lalit ghongade <lalitghongade1451999@gmail.com>
2023-08-15 02:30:47 +0530

    removed unused imports

--- starter/src/main/java/com/lala/starter/auth/CorsConfiguration.java
+++ starter/src/main/java/com/lala/starter/auth/CorsConfiguration.java
@@ -1,9 +1,7 @@
 package com.lala.starter.auth;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
